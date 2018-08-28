9b8af7b377a03868cc49e5ac7d9e5dea6778aebb
Mia Shi <miashi142857@gmail.com>
2018-08-28 12:33:50 -0700

    php

---
+++ contactform.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_POST['Submit'])) {
+
+
+  $name = $_POST['name'];
+  $mailFrom = $_POST['email'];
+  $message = $_POST['message'];
+
+  $mailTo = "miayumengs@yahoo.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "New Form Submission From cmai.ca".$name.".\n\n".$message;
+
+
+  mail ($mailTo,$mailFrom, $txt, $headers);
+header("Location: contact.php?mailsend");
+
+}
+
+?>
